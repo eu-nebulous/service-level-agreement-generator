7b150370fd59e1fa8b44d213e91facdd9c8811fe
vaggbarmpas@gmail.com <evbarmpas@seerc.org>
2025-04-08 11:13:32 +0300

    Updated UI

--- src/main/java/org/seerc/nebulous/sla/rest/SLAServeUI.java
+++ src/main/java/org/seerc/nebulous/sla/rest/SLAServeUI.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class SLAServeUI {
 	@GetMapping({"/index", "/"})
 	public String greeting(@RequestParam(name="appId", required = true) String applicationId, @RequestParam(name="nonce", required = true) String nonce ) {
-		System.out.println(applicationId);
-		System.out.println(nonce);
 		return "index";
 	}
 	
