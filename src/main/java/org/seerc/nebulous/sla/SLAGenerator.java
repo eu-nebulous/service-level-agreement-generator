e5a4cb688e28d8a5cf188e98232f7c883c344afb
ebarmpas <56959801+ebarmpas@users.noreply.github.com>
2025-05-14 15:30:18 +0300

    Update SLAGenerator.java (#26)

--- src/main/java/org/seerc/nebulous/sla/SLAGenerator.java
+++ src/main/java/org/seerc/nebulous/sla/SLAGenerator.java
@@ -11,6 +11,10 @@ public class SLAGenerator {
 	
 	
 	public static void main(String[] args) {
+
+		for(int i = 0; i < args.length; i++) {
+			System.out.println("args: " + args[i]);
+		}
 //		SALConnection.getInstance(args[1]);
 		OntologyConnection.getInstance(args[0]);
 		SpringApplication.run(SLAGenerator.class, args);
