885f9b735956fe563d5c1d0326d7dba08f8e163c
vaggbarmpas@gmail.com <evbarmpas@seerc.org>
2025-10-08 13:24:52 +0300

    Current version

--- src/main/java/org/seerc/nebulous/sla/rest/EXNConnection.java
+++ src/main/java/org/seerc/nebulous/sla/rest/EXNConnection.java
@@ -4,13 +4,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.qpid.protonj2.client.Message;
 import org.seerc.nebulous.sla.components.SLA;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import eu.nebulouscloud.exn.Connector;
+import eu.nebulouscloud.exn.core.Consumer;
 import eu.nebulouscloud.exn.core.Context;
+import eu.nebulouscloud.exn.core.Handler;
 import eu.nebulouscloud.exn.core.Publisher;
 import eu.nebulouscloud.exn.core.SyncedPublisher;
 import eu.nebulouscloud.exn.handlers.ConnectorHandler;
@@ -23,6 +26,7 @@ public class EXNConnection {
 	private static Connector conn;
 	private static SyncedPublisher getFromUi;
 	private static Publisher postToSla;
+	private static Consumer c;
 	
 	private EXNConnection() {
 		
@@ -38,9 +42,16 @@ public class EXNConnection {
 
 		getFromUi = new SyncedPublisher("eu-app-get-publisher", "eu.nebulouscloud.ui.app.get", true, true);
 		postToSla= new Publisher("eu-ontology-sla-publisher", "eu.nebulouscloud.ontology.sla", true, true);
-
-		conn = new Connector("eu", h , List.of(getFromUi, postToSla), List.of(), new StaticExnConfig("nebulous-activemq",5672,"admin","admin",5));
-
+		c = new Consumer ("eu-ontology-bqa", "ontology.bqa", new Handler() {
+			@Override
+		
+			public void onMessage(String key, String address, Map body, Message message, Context context) {
+				System.out.println(body);
+			}
+			
+		});
+		conn = new Connector("eu.nebulouscloud", h , List.of(getFromUi, postToSla), List.of(c), new StaticExnConfig("localhost",5672,"admin","admin",5));
+		
 		conn.start();
 
 	}
@@ -57,7 +68,6 @@ public class EXNConnection {
 	
 	public void publishSLA(SLA sla) {
 		Map m = new HashMap(new ObjectMapper().convertValue(sla, new TypeReference<Map<String, Object>>() {}));
-
 		postToSla.send(m, sla.getSlaName(), null, false);
 	}
 }
