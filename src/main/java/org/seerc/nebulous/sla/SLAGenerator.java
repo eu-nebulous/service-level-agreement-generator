1029b787d62c4a3dfdf8cc7d5570ca7370783ad1
ebarmpas <56959801+ebarmpas@users.noreply.github.com>
2025-11-28 13:50:44 +0200

    Update SLAGenerator.java

--- src/main/java/org/seerc/nebulous/sla/SLAGenerator.java
+++ src/main/java/org/seerc/nebulous/sla/SLAGenerator.java
@@ -14,7 +14,7 @@ public class SLAGenerator {
 	public static void main(String[] args) {
 //		SALConnection.getInstance(args[1]);
 		String ontUrl = "http://nebulous-ontology-server:80";
-		String bqaUrl = "http://nebulous-brokerage-quality-assurance-server :8080";
+		String bqaUrl = "http://nebulous-brokerage-quality-assurance-server:8080";
 		OntologyConnection.getInstance(ontUrl, bqaUrl);
 		EXNConnection.getInstance();
 		SpringApplication.run(SLAGenerator.class, args);
